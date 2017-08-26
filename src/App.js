1c80a0616455c8af48f712cf2313dccf88190a69
OkYesAnap <OkYesAnap@gmail.com>
2017-08-26 21:39:28 +0300

    login

--- src/App.js
+++ src/App.js
@@ -1,66 +1,40 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
-import Search from './components/search';
-import VideoTitleDescription from './components/video-title-description'
-import ListItem from './components/list-item'
-import YTSearch from 'youtube-api-search';
-const API_KEY = 'AIzaSyC1ORL6Y3zxvLLev6QHUqP8eF1hFbYo1WI';
+import './theme/App.css';
+import Header from './components/header/header';
+import Router from './router';
+import Footer from './components/footer/footer';
+import Signin from './components/auth/signin';
+import IndexPage from './components/index/indexpage';
+import Signup from './components/auth/signup';
+import NotFoud from './components/notfound'
 
+var components = {
+  IndexPage,
+  Signin,
+  Signup,
+  NotFoud
+} 
 
 class App extends Component {
   constructor() {
     super();
-    this.searchingData = {};
-    this.state = { search: 'its a live!', autoplay: "?autoplay=1", currentVideo: 0, imgData: [{}, {}, {}, {}, {}] };
-  }
-  upDate() {
-    YTSearch({ key: API_KEY, term: this.state.search },
-      data => {
-        this.setState({
-          imgData: data.map((val, index) => {
-            let { snippet } = data[index];
-            return ({
-              videoId: data[index].id.videoId,
-              title: snippet.title,
-              description: snippet.description,
-              thumbnailsURL: snippet.thumbnails.default.url
-            })
-          })
-        });
-      });
-  }
-  searchChange(e) {
-    this.setState({ search: e.target.value, autoplay:"" });
-    this.upDate();
-  }
-  videoChoice(e){
-    this.setState({currentVideo: (e.target.id ? e.target.id : this.elemIdFinder(e.target)), autoplay:""});
-  }
-  elemIdFinder(elem){
-    return(elem.parentElement.id ? elem.parentElement.id : this.elemIdFinder(elem.parentElement));
-  }
+    this.state = {
+      name: 'Andrey',
+      password: 'Parol'
+    }
+}
   render() {
-    this.upDate();
+    let {name, password} = this.state;
     return (
-      <main className="container">
-        <Search
-          searchChange={this.searchChange.bind(this)}
-          searchString={this.state.search}
+      <main className="App">
+        <Header />
+        <Router         
+        name={name}
+        password={password}
+        components={components}
         />
-        <div className="row">
-          <VideoTitleDescription
-            autoplay={this.state.autoplay}
-            imgData={this.state.imgData[this.state.currentVideo]} />
-          <ul className="col-md-4 list-group">
-            {this.state.imgData.map((data, index) => {
-              return <ListItem 
-              imgData={this.state.imgData[index]}
-              currentVideo={index}
-              videoChoice={this.videoChoice.bind(this)} />
-            })}
-          </ul>
-        </div>
+        <Footer />
       </main>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
-import './index.css';
+import './theme/index.css';
 import App from './App';
+import {BrowserRouter as Router} from 'react-router-dom'
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const render = Component => ReactDOM.render(
+    <Router>
+        <App />
+    </Router>, document.getElementById('root'));
+render(App);
+
+if(module.hot){
+    console.log('Updated');
+    module.hot.accept('./App', () => render(App));
+}
 registerServiceWorker();
